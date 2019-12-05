8d6fc182c118f4f5f39180531ed4116daeb2fff8
Desy Amelina Suryani <56750173+desyamelinas@users.noreply.github.com>
2019-12-04 19:56:09 -0800

    data_tables_desy

---
+++ application/views/template/v_footer.php
@@ -0,0 +1,58 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0
+      </div>
+      <strong>Copyright &copy; 2019 161240000589.</strong> 
+    </div>
+</footer>
+
+
+<!-- jQuery 2.2.3 -->
+<script src="<?=base_url()?>assets/js/jquery.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="<?=base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="<?=base_url()?>assets/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="<?=base_url()?>assets/plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=base_url()?>assets/dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?=base_url()?>assets/dist/js/demo.js"></script>
+<!-- DataTables -->
+<script src="<?=base_url()?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?=base_url()?>assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
+<!-- jQuery Knob -->
+<script src="<?=base_url()?>assets/plugins/knob/jquery.knob.js"></script>
+<!-- Sparkline -->
+<script src="<?=base_url()?>assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+<!-- FLOT CHARTS -->
+<script src="<?=base_url()?>assets/plugins/flot/jquery.flot.min.js"></script>
+<!-- FLOT RESIZE PLUGIN - allows the chart to redraw when the window is resized -->
+<script src="<?=base_url()?>assets/plugins/flot/jquery.flot.resize.min.js"></script>
+<!-- FLOT PIE PLUGIN - also used to draw donut charts -->
+<script src="<?=base_url()?>assets/plugins/flot/jquery.flot.pie.min.js"></script>
+<!-- FLOT CATEGORIES PLUGIN - Used to draw bar charts -->
+<script src="<?=base_url()?>assets/plugins/flot/jquery.flot.categories.min.js"></script>
+<!-- Page script -->
+<!-- page script -->
+<script type="text/javascript">
+  $(function() {
+
+    $('#msg').slideDown('slow').delay(4000).slideUp('slow');
+  });
+</script>
+
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+   
+    });
+
+});
+</script>
+<script>   
+  $('#notifications').slideDown('slow').delay(2000).slideUp('slow');
+</script>
---
+++ application/views/v_dashboard.php
@@ -0,0 +1,22 @@
+<div class="content-wrapper">
+  <div class="container">
+		<section class="content">
+      <div class="col-md-12">
+        <div class="box box-warning box-solid">
+          <div class="box-header with-border">
+            <h3 class="box-title">Dashboard</h3>
+					<div class="box-tools pull-right">
+						<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+						</button>
+					</div>
+					</div>
+           	<div class="box-body">
+             	<h1><center>Selamat Datang Di Data Mahasiswa</center></h1>
+        
+            </div>
+            <!-- /.box-footer -->
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
